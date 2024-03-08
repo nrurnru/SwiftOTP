bf040c26e744ea3b5c6b471e3ad6d54b21a3a504
nrurnru <nrurnru@gmail.com>
2024-03-08 19:35:09 +0900

    Add privacy manifest to SPM
    added Package@swift-5.9 for support resources.

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "SwiftOTP",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "SwiftOTP", targets: ["SwiftOTP"]),
+        .library(name: "SwiftOTP-Dynamic", type: .dynamic, targets: ["SwiftOTP"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" 
+..< "4.0.0")
+    ],
+    targets: [
+        .target(name: "SwiftOTP", dependencies: ["Crypto"], path: "SwiftOTP/", resources: [.copy("PrivacyInfo.xcprivacy")]),
+        .testTarget(name: "SwiftOTPTests", dependencies: ["SwiftOTP"])
+    ]
+)
